ababa6a95d3edc225934b5a9b6e313beb199251d
eaap <apesme2006@gmail.com>
2026-04-30 16:58:24 +0200

    Project/repo creation

---
+++ src/main/java/com/gft/warehouse/warehouseworkshop/WarehouseWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.gft.warehouse.warehouseworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseWorkshopApplication.class, args);
+    }
+
+}

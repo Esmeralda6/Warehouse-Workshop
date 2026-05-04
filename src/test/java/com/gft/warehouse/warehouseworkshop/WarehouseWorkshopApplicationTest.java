a742e8065d6b1bc33339d5b9e50700175a6a3988
JEIJ <jeij@gft.com>
2026-05-04 12:01:53 +0200

    test: project changes to pass initial tests

---
+++ src/test/java/com/gft/warehouse/warehouseworkshop/WarehouseWorkshopApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gft.warehouse.warehouseworkshop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WarehouseWorkshopApplicationTest {
+    @Test
+    void test1(){
+        Assertions.assertThat(true).isTrue();
+    }
+
+
+}

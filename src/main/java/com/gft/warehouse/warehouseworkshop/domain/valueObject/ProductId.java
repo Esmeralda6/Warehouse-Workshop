35ca1861aa7865fe128147edf072b127490bf38b
eaap <apesme2006@gmail.com>
2026-05-05 16:20:54 +0200

    feat: ProductId and Quantity validations

--- src/main/java/com/gft/warehouse/warehouseworkshop/domain/valueObject/ProductId.java
+++ src/main/java/com/gft/warehouse/warehouseworkshop/domain/valueObject/ProductId.java
@@ -1,12 +1,26 @@
 package com.gft.warehouse.warehouseworkshop.domain.valueObject;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ProductId {
     private final String id;
 
     public ProductId(String id) {
-        this.id = id;
+
+        this.id = validateProductId(id);
+    }
+
+    private String validateProductId (String id){
+        if (id == null){
+            throw new IllegalArgumentException("The ID of the product can't be empty.");
+        }
+
+        if ( id.isBlank() ){
+            throw new IllegalArgumentException("The ID of the product can't be blank.");
+        }
+        return id;
     }
 }
--- src/main/java/com/gft/warehouse/warehouseworkshop/domain/valueObject/Quantity.java
+++ src/main/java/com/gft/warehouse/warehouseworkshop/domain/valueObject/Quantity.java
@@ -1,12 +1,21 @@
 package com.gft.warehouse.warehouseworkshop.domain.valueObject;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class Quantity {
     private final int quantity;
 
     public Quantity(int quantity) {
-        this.quantity = quantity;
+        this.quantity = validateQuantity(quantity);
+    }
+
+    private int validateQuantity(int quantity){
+        if (quantity < 0){
+            throw new IllegalArgumentException("The quantity can't be negative.");
+        }
+        return quantity;
     }
 }

71144e4777decf5207ede6c79f91000cabb72fdf
eaap <apesme2006@gmail.com>
2026-05-06 16:42:41 +0200

    feat: create and implementation of stock exception

--- src/main/java/com/gft/warehouse/warehouseworkshop/domain/aggregates/StockItem.java
+++ src/main/java/com/gft/warehouse/warehouseworkshop/domain/aggregates/StockItem.java
@@ -1,5 +1,6 @@
 package com.gft.warehouse.warehouseworkshop.domain.aggregates;
 
+import com.gft.warehouse.warehouseworkshop.domain.exceptions.InsuficientStockException;
 import com.gft.warehouse.warehouseworkshop.domain.valueObject.ProductId;
 import com.gft.warehouse.warehouseworkshop.domain.valueObject.Quantity;
 import lombok.Builder;
@@ -21,7 +22,7 @@ public class StockItem {
 
     public void subtract(Quantity qty) {
         if (qty.getQuantity() > this.quantity.getQuantity()) {
-            throw new IllegalArgumentException("Not enough stock to subtract");
+            throw new InsuficientStockException("Not enough stock to subtract");
         }
         this.quantity = Quantity.builder().quantity(this.quantity.getQuantity() - qty.getQuantity()).build();
     }
@@ -29,4 +30,6 @@ public class StockItem {
     public boolean hasProduct(ProductId productId) {
         return this.productId.getId().equals(productId.getId());
     }
+
+    
 }

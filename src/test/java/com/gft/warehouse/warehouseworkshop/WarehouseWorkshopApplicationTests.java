d81bbc0351c114741465cdce98a26bc27cba6446
JEIJ <jeij@gft.com>
2026-05-15 09:35:08 +0200

    refactor: removed unused class and tests

--- src/test/java/com/gft/warehouse/warehouseworkshop/WarehouseWorkshopApplicationTests.java
+++ src/test/java/com/gft/warehouse/warehouseworkshop/WarehouseWorkshopApplicationTests.java
@@ -1,15 +1,11 @@
 package com.gft.warehouse.warehouseworkshop;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 class WarehouseWorkshopApplicationTests {
 
     @Test
-    void test1() {
-        Assertions.assertThat(true).isTrue();
+    void contextLoads() {
     }
 
 }

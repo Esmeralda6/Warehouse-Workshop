47ca153e9e197afa6c7a8f077cee643d0a4542ca
JEIJ <jeij@gft.com>
2026-05-21 10:09:49 +0200

    refactor: refactor warehouseRegistered event publisher

--- src/main/java/com/gft/warehouse/warehouseworkshop/infrastructure/messaging/publisher/RabbitMQEventPublisher.java
+++ src/main/java/com/gft/warehouse/warehouseworkshop/infrastructure/messaging/publisher/RabbitMQEventPublisher.java
@@ -1,6 +1,8 @@
 package com.gft.warehouse.warehouseworkshop.infrastructure.messaging.publisher;
 
+import com.gft.warehouse.warehouseworkshop.domain.aggregates.Warehouse;
 import com.gft.warehouse.warehouseworkshop.domain.events.DomainEvent;
+import com.gft.warehouse.warehouseworkshop.domain.events.WarehouseCreatedEvent;
 import com.gft.warehouse.warehouseworkshop.domain.ports.EventPublisher;
 import com.gft.warehouse.warehouseworkshop.infrastructure.messaging.config.RabbitMQConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,4 +22,17 @@ public class RabbitMQEventPublisher implements EventPublisher {
     public void publish(DomainEvent event) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, event.getEventType(), event);
     }
+
+    @Override
+    public void warehouseRegistered( Warehouse warehouse) throws Exception{
+        warehouse.recordEvent(new WarehouseCreatedEvent(
+                warehouse.getWarehouseId().getId().toString(),
+                warehouse.getWarehouseName(),
+                warehouse.getWarehouseLocation(),
+                warehouse.getWarehouseType().name()
+        ));
+        warehouse.getDomainEvents().forEach(this::publish);
+        warehouse.clearDomainEvents();
+    }
+
 }
